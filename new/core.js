17abc15d79e4cf13289afb043b50d297b15cc50b
Caio <caio@debian.debian>
2019-02-08 14:19:38 -0200

    Processador JS criado

--- new/core.js
+++
@@ -1,7 +0,0 @@
-function carregaFooter() {
-    $('#dataJogo').text("2014 - " + (new Date()).getFullYear());
-}
-
-function alertaisso() {
-    alert("cu");
-}
--- new/src/js/core.js
+++ new/src/js/core.js
@@ -2,6 +2,41 @@ function carregaFooter() {
     $('#dataJogo').text("2014 - " + (new Date()).getFullYear());
 }
 
+function gameOver(hasVencido, numeroDesafio) {
+    var mensagem = "";
+    $("#tempo").css('display', 'none');
+    $("#botao").css('display', 'none');
+    $('#exibePalavra').css('display', 'none');
+    
+    if(hasVencido) {
+        mensagem = `
+            <p>Vocês venceram!</p>
+            <p class="pb-5"><span class="far fa-grin-beam h1"></span></p>
+        `;
+    } else {
+        mensagem = `
+            <p>Vocês perderam!</p>
+            <p class="pb-5"><span class="far fa-dizzy h1"></span></p>
+        `;
+    }
+    var link = (numeroDesafio==1) ? ("primeiro.html") : ((numeroDesafio==2) ? ("segundo.html") : ("terceiro.html"));
+    mensagem += `
+        <p>
+            <a class="btn btn-light btn-lg" role="button" href='${link}'><span class="far fa-play-circle"></span> Jogar novamente</a>
+        </p>
+    `;
+
+    $('#mensagemPalavra').html(mensagem);
+
+    /*
+     * retorna falso porque a variável responsável por ver se o jogo ainda está
+     * acontecendo é quem chama essa função; como esta função indica o fim de
+     * jogo, a variável responsável por ver se o jogo ainda está acontecendo
+     * deve ser tornada falsa, ou seja, deve ser inativada.
+     */
+    return false;
+}
+
 /*
  * Esta é uma implementação do algoritmo Fisher-Yates para sortear um vetor
  * com valores aleatórios.
@@ -19,16 +54,81 @@ function randomizer(vetor) {
     return vetor;
 }
 
-function carregaPalavras() {
+function processaJogo(dados, informacoes) {
     var palavras = new Array();
-    $.ajax({
-        type: "GET",
-        async: false,
-        url: "src/senhas.json",
-        success: function(dados) {
-           $.each(dados, function(chave, valor) {
-                palavras.push(valor);
-           });
+
+    /*
+     * Este for irá pegar somente as palavras cujo índice
+     * seja igual ao do desafio corrente. Como o desafio
+     * começa do 1, e o índice do 0, teve-se de decrementar
+     * em 1 o desafio.
+     */
+    for(i=0;i<=informacoes.desafio-1;i++) {
+        palavras.push(dados[i]);
+    }
+
+    /* pega as palavras relativas ao nivel em que se está */
+    var palavrasNivel = palavras[informacoes.desafio-1][informacoes.nivel-1];
+
+    /*
+     * Se o desafio não for o primeiro, e se o nível for o
+     * primeiro, deve-se pegar as palavras tanto do nível 1
+     * do desafio atual, quanto de todos os níveis dos
+     * desafios anteriores.
+     */
+    if(informacoes.desafio!=1 && informacoes.nivel==1) {
+        /*
+         * DesafioTmp é uma variável temporária, que será
+         * usada para pegar o índice dos desafios
+         * anteriores; por isso ela é desafio-2; pois, caso
+         * se esteja no desafio 2, ela pegará o índice (0)
+         * do primeiro desafio.
+         */
+        var desafioTmp = informacoes.desafio-2;
+
+        /*
+         * Insere palavras de desafios anteriores às
+         * palavras desse nível.
+         */
+        while(desafioTmp>=0) {
+            var tam1 = palavras[desafioTmp][0].length;
+            var tam2 = palavras[desafioTmp][1].length;
+            var tam3 = palavras[desafioTmp][2].length;
+
+            for(i=0;i<tam1;i++) {
+                palavrasNivel.push(palavras[desafioTmp][0][i]);
+            }
+            for(i=0;i<tam2;i++) {
+                palavrasNivel.push(palavras[desafioTmp][1][i]);
+            }
+            for(i=0;i<tam3;i++) {
+                palavrasNivel.push(palavras[desafioTmp][2][i]);
+            }
+            desafioTmp--;
         }
-    });
+    }
+    
+    /*
+     * Sorteia a ordem das palavras. Depois, remove, dentre
+     * elas, aquelas que já foram usadas previamente. Aí,
+     * ele adiciona a palavra da vez na lista de palavras
+     * já utilizadas. Depois, mostra a palavra da vez na
+     * tela.
+     */
+
+    palavrasNivel = randomizer(palavrasNivel);
+    palavrasNivel = palavrasNivel.filter(palavra => !informacoes.palavrasUsadas.includes(palavra));
+    informacoes.palavrasUsadas.push(palavrasNivel[0]);
+
+    $("#exibePalavra").text(palavrasNivel[0] + ".");
+
+    /*
+     * Avança à próxima palavra e, se passar da terceira,
+     * reinicia para a primeira palavra do próximo nível.
+     */
+    informacoes.numero++;
+    if(informacoes.numero==4) {
+        informacoes.numero=1;
+        informacoes.nivel++;
+    }
 }
